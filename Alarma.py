97e6ac517e5e4cdd7594d0ff613f4a7d79ffe26b
Miguel Gomez <16030461@itcelaya.edu.mx>
2020-02-17 11:56:39 -0600

    Add files via upload

---
+++ Alarma.py
@@ -0,0 +1,16 @@
+import time 
+global r                            # se hace una variable global
+
+class persona:
+    def __init__(self, persona,nadie):    
+        self.persona=p
+        self.nadie=n
+
+    def persona(self):
+        global r
+        r=persona
+        print(r)
+class alarma:
+    def   
+    
+
